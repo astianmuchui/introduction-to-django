285231ff2450b3595a4a29cbd6cbb3cc0a2ff8f2
astianmuchui <sebastianmuchui79@gmail.com>
2023-05-09 12:30:01 +0300

    added 404 template

--- tennis_club/settings.py
+++ tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e4&#0gife!=ji&k1ya@$q*)#qpg008#wn88sbzm+n-7cv*ie^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
