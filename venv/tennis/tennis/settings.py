d72ea64d54150398c878b993f6601375229deb52
astianmuchui <sebastianmuchui79@gmail.com>
2023-06-22 15:37:12 +0300

    Static file config

--- venv/tennis/tennis/settings.py
+++ venv/tennis/tennis/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',# Whitenoise library
 ]
 
 ROOT_URLCONF = 'tennis.urls'
@@ -116,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 # Default primary key field type
