f4cf4c55645bed9935d3e3bbe1f3093cd3f0aeb8
astianmuchui <sebastianmuchui79@gmail.com>
2023-07-01 09:48:59 +0300

    Updated views and added some comments

--- venv/tennis/users/views.py
+++ venv/tennis/users/views.py
@@ -5,7 +5,7 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-
+    # Check for submit
     if request.POST:
         save = models.User(uname=request.POST.get('name'), email=request.POST.get('email'))
         save.save()
@@ -14,9 +14,12 @@ def index(request):
 
 def users(request):
 
+    # FETCH all users order by ido
     usrs = models.User.objects.all().order_by('-id')
+
     template = loader.get_template('view.html')
     context = {
         'users': usrs,
     }
+
     return HttpResponse(template.render(context, request))
